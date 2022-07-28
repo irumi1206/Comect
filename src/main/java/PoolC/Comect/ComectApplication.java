0ad58838b5788d338e257c00bcb8b26a924eccfd
jimmy0006 <jinmin645@gmail.com>
2022-07-28 16:54:41 +0900

    new files

---
+++ src/main/java/PoolC/Comect/ComectApplication.java
@@ -0,0 +1,13 @@
+package PoolC.Comect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PoolC/Comect/ComectApplicationTests.java
@@ -0,0 +1,13 @@
+package PoolC.Comect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

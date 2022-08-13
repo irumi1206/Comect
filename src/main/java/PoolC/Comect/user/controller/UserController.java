7619cd1a34ae21a8e50a31519ab19f682e746385
jimmy0006 <jinmin645@gmail.com>
2022-08-13 22:59:04 +0900

    asdf

--- src/main/java/PoolC/Comect/user/controller/UserController.java
+++ src/main/java/PoolC/Comect/user/controller/UserController.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller @ResponseBody
+@Controller
+@ResponseBody
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;

e1b521ed9db0f7c2b73c9a47a68621061c57ec96
irumi1206 <sungha.kim123@gmail.com>
2022-08-21 07:09:34 +0900

    solving ec2 error

--- src/main/java/PoolC/Comect/ComectApplication.java
+++ src/main/java/PoolC/Comect/ComectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"PoolC.Comect.data"})
 public class ComectApplication {
 
 	public static void main(String[] args) {

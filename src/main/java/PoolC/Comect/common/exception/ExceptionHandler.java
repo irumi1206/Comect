9c3c1a17f589dc75501eb99b36f345d87b860681
irumi1206 <sungha.kim123@gmail.com>
2022-10-07 15:15:31 +0900

    handling 400

--- src/main/java/PoolC/Comect/common/exception/ExceptionHandler.java
+++ src/main/java/PoolC/Comect/common/exception/ExceptionHandler.java
@@ -1,9 +1,15 @@
 package PoolC.Comect.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
@@ -14,5 +20,11 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
         log.error("Error! Code: "+e.getErrorCode());
         return ErrorResponse.toResponseEntity(e.getErrorCode());
     }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Void> arguementHandler(MethodArgumentNotValidException e){
+        return ResponseEntity.badRequest().build();
+    }
+
 
 }
--- src/main/java/PoolC/Comect/common/infra/JwtAuthenticationFilter.java
+++ src/main/java/PoolC/Comect/common/infra/JwtAuthenticationFilter.java
@@ -28,6 +28,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
             // SecurityContext 에 Authentication 객체를 저장합니다.
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
+
         chain.doFilter(request, response);
     }
 }

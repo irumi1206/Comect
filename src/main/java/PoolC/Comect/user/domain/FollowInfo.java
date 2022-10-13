926a1f9e3d1dc02532f5d56962f776c269cd4a0e
jimmy0006 <jinmin645@gmail.com>
2022-10-13 21:48:33 +0900

    isFollowing

--- src/main/java/PoolC/Comect/user/domain/FollowInfo.java
+++ src/main/java/PoolC/Comect/user/domain/FollowInfo.java
@@ -9,9 +9,9 @@ public class FollowInfo {
     private String email;
     private String nickname;
     private String imageUrl;
-    private boolean isFollowing;
+    private Boolean isFollowing;
 
-    public FollowInfo(String email, String nickname, String imageUrl, boolean isFollowing) {
+    public FollowInfo(String email, String nickname, String imageUrl, Boolean isFollowing) {
         this.email = email;
         this.nickname = nickname;
         this.imageUrl = imageUrl;

dcb6b90704d772cab51456c19c1c89f9a9dae8fb
jimmy0006 <jinmin645@gmail.com>
2022-11-03 23:24:11 +0900

    password change regex

--- src/main/java/PoolC/Comect/user/dto/PasswordChangeCheckRequestDto.java
+++ src/main/java/PoolC/Comect/user/dto/PasswordChangeCheckRequestDto.java
@@ -2,9 +2,12 @@ package PoolC.Comect.user.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Size;
+
 @Data
 public class PasswordChangeCheckRequestDto {
     private String email;
     private int randomNumber;
+    @Size(min=6,max=15)
     private String newPassword;
 }
